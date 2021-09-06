273a495db3dd27258fd00e2f80044320be1aafa2
Luca Naterop <luca.naterop@psi.ch>
2021-09-06 16:49:01 +0200

    redesign diagnostic panel

--- velokino-monitor/App.js
+++ velokino-monitor/App.js
@@ -5,13 +5,40 @@
 //   document.getElementById('sup').innerHTML = data
 // });
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function getCorrectColor(value) {
+  if(value < 1.0) return '#b43535'
+  else if (value < 3) return '#b2bd18'
+  else return '#2d9c23'
+}
+
+
+function updateUI(data) {
+  // updates the UI with latest data
+  const divs = document.getElementsByClassName('velopanel');
+  const divArray = [].slice.call(divs);
+  divArray.forEach((div, i) => {
+    div.innerHTML =  data[i] + ' A'
+    $(div).css('background-color',getCorrectColor(data[i]))
+  })
+
+}
+
 setInterval(() => {
 
   var data = [];
   for(i = 0; i<10; i++) {
-    data.push(Math.random())
+    const val = Math.round(Math.random()*50)/10
+    data.push(val)
   }
+  updateUI(data)
 
-  
-  
-},100)
+},300)
